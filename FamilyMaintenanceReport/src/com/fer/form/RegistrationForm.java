f51807b079717209cc864dee5eda1d365376085b
Naveen T <Naveen T@DESKTOP-LPOHANU>
2019-12-02 17:10:36 +0530

    Family expense report

---
+++ FamilyMaintenanceReport/src/com/fer/form/RegistrationForm.java
@@ -0,0 +1,59 @@
+package com.fer.form;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.validator.ValidatorForm;
+
+public class RegistrationForm extends ValidatorForm{
+	
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String userName;
+	private String password;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
